267ee19546d926d0e6272ceb562016570c7d9655
Anezijane <ahnnieanezi@gmail.com>
2024-03-07 14:53:36 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import ReferralComponent from './Components/ReferralComponent';
+
 
 function App() {
     return (
         <>
             <p className='text-4xl font-bold underline'>Hello world!</p>
+            <ReferralComponent />
         </>
     );
 }
