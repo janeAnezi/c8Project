579a0c6e8d251399a4abceb68451721155ce6608
Anezijane <ahnnieanezi@gmail.com>
2024-03-07 21:00:45 +0100

    add content to referral section

--- src/Components/ReferralComponent.jsx
+++ src/Components/ReferralComponent.jsx
@@ -1,7 +1,11 @@
 export default function ReferralComponent() {
   return(
     <>
-      
+      <div className="text-left inline-block w-80 bg-slate-100 border p-4 rounded-xl mt-4">
+            <p className="text-bold">Invite a friend</p>
+            <p className="text-sm mb-3 ">Earn 10 points for every friend that signs up.</p>
+            
+        </div>
     </>
   )
 }
--- src/Components/points.jsx
+++ src/Components/points.jsx
@@ -1,11 +1,11 @@
 export default function Points() {
     return(
       <>
-        <div className="container text-left inline-block w-80 bg-slate-100 border p-4 rounded-xl">
+        <div className=" text-left inline-block w-80 bg-slate-100 border p-4 rounded-xl">
             <h1 className="text-3xl my-2"><span className="font-bold">0</span> Points</h1>
-            <p className="text-base mb-3 ">Reach 400 points and get a meal on us!</p>
+            <p className="text-sm mb-3 ">Reach 400 points and get a meal on us!</p>
             <button className="bg-black text-white rounded-md px-2 pb-1">Redeem Points</button>
-        </div>
+        </div><br></br>
       </>
     )
   }
