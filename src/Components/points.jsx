665809f20032942fa011239fa3d118677eeebbe2
Anezijane <ahnnieanezi@gmail.com>
2024-03-07 21:06:03 +0100

    add margin and font weight to components

--- src/Components/points.jsx
+++ src/Components/points.jsx
@@ -2,7 +2,7 @@ export default function Points() {
     return(
       <>
         <div className=" text-left inline-block w-80 bg-slate-100 border p-4 rounded-xl">
-            <h1 className="text-3xl my-2"><span className="font-bold">0</span> Points</h1>
+            <h1 className="text-3xl mb-2"><span className="font-semibold">0</span> Pts</h1>
             <p className="text-sm mb-3 ">Reach 400 points and get a meal on us!</p>
             <button className="bg-black text-white rounded-md px-2 pb-1">Redeem Points</button>
         </div><br></br>
