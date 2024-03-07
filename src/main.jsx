a1eed4aef041c7e5ba5a0d535390e0f2e194abaf
Anezijane <ahnnieanezi@gmail.com>
2024-03-08 00:55:52 +0100

    import and initialize firebase in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebaseConfig'; // Import your Firebase configuration
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from './firebaseConfig'; // Import your Firebase configuration
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+import 'firebase/auth'; // For Firebase Authentication
+import 'firebase/firestore';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
