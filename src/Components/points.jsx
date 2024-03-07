2f8c3779bb39bca6c394c8f16db12bad2b5bceb1
Anezijane <ahnnieanezi@gmail.com>
2024-03-07 20:44:06 +0100

    add style to points component

--- src/Components/points.jsx
+++ src/Components/points.jsx
@@ -1,10 +1,10 @@
 export default function Points() {
     return(
       <>
-        <div className="container inline-block w-80 bg-slate-100 border border-3 m-2 p-4 rounded-xl">
-            <h1 className="text-xl"><span>0</span> Points</h1>
-            <p>Reach 400 points and get a meal on us!</p>
-            <button>Redeem Points</button>
+        <div className="container text-left inline-block w-80 bg-slate-100 border p-4 rounded-xl">
+            <h1 className="text-3xl my-2"><span className="font-bold">0</span> Points</h1>
+            <p className="text-base mb-3 ">Reach 400 points and get a meal on us!</p>
+            <button className="bg-black text-white rounded-md px-2 pb-1">Redeem Points</button>
         </div>
       </>
     )
