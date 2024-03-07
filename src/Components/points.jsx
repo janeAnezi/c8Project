082480a5657c361ed7d21a6eef32a729c035cb99
Anezijane <ahnnieanezi@gmail.com>
2024-03-07 20:08:56 +0100

    add background and width to points component

--- src/Components/points.jsx
+++ src/Components/points.jsx
@@ -1,10 +1,10 @@
 export default function Points() {
     return(
       <>
-        <div className="container bg-slate-100">
-          <h1><span>0</span> Points</h1>
-          <p>Reach 400 points and get a meal on us!</p>
-          <button>Redeem Points</button>
+        <div className="container inline-block w-80 bg-slate-100 border border-3 m-2 p-4 rounded-xl">
+            <h1 className="text-xl"><span>0</span> Points</h1>
+            <p>Reach 400 points and get a meal on us!</p>
+            <button>Redeem Points</button>
         </div>
       </>
     )
