ec614fbbb3d29695d63dfcad3d59e21f1a2c1cf4
Anezijane <ahnnieanezi@gmail.com>
2024-03-07 21:23:32 +0100

    add button and input type text to referral component

--- src/Components/points.jsx
+++ src/Components/points.jsx
@@ -1,7 +1,7 @@
 export default function Points() {
     return(
       <>
-        <div className=" text-left inline-block w-80 bg-slate-100 border p-4 rounded-xl">
+        <div className=" text-left inline-block w-80 bg-slate-100 border pl-4 py-2 rounded-xl">
             <h1 className="text-3xl mb-2"><span className="font-semibold">0</span> Pts</h1>
             <p className="text-sm mb-3 ">Reach 400 points and get a meal on us!</p>
             <button className="bg-black text-white rounded-md px-2 pb-1">Redeem Points</button>
