a851167ed6c56dba194fde3f72aaf212c16aed61
Anezijane <ahnnieanezi@gmail.com>
2024-03-08 22:31:48 +0100

    add condition to input value to display referral link on login

--- src/Components/Referral.jsx
+++ src/Components/Referral.jsx
@@ -5,6 +5,12 @@ export default function Referral() {
     const [referralLink, setReferralLink] = useState('');
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+    // set loggedin state to true to automatically generate referral link
+    useEffect(() => {
+        setIsLoggedIn(true);
+        generateReferralLink();
+      }, []);
+
     // to generate the referral link
     const generateReferralLink = async () => {
         try {
@@ -53,19 +59,23 @@ export default function Referral() {
             <div className="text-left inline-block w-80 bg-slate-100 border pl-4 py-2 rounded-xl mt-4">
                 <p className="font-semibold mb-2">Invite a friend</p>
                 <p className="text-sm mb-3 ">Earn 10 points for every friend that signs up.</p>
+                {isLoggedIn && (
                 <div className="bg-white border rounded-lg inline-block w-[270px] relative px-2 py-2">
                     <input
-                        id="referralLink"
-                        className="placeholder:text-blue-700 text-sm inline-block w-[200px] text-blue-700"
-                        type="text"
-                        placeholder="https://example.com/blessing"
-                        value={referralLink}
-                        readOnly
+                    id="referralLink"
+                    className="placeholder:text-blue-700 text-sm inline-block w-[200px] text-blue-700"
+                    type="text"
+                    placeholder="https://example.com/blessing"
+                    value={referralLink}
+                    readOnly
                     />
                     <button onClick={copyReferralLink} className="bg-black text-white rounded-lg text-sm pb-1 px-1 absolute right-2">
-                        Copy
+                    Copy
                     </button>
                 </div>
+                )}
+                {!isLoggedIn && <p>Please log in to see your referral link.</p>}
+                
             </div>
         </>
     );
