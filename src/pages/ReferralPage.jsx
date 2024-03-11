a27c33835707b9eed4858626ecaa5e38cceac407
Anezijane <ahnnieanezi@gmail.com>
2024-03-11 12:32:01 +0100

    create pages folder and referralpage file

---
+++ src/pages/ReferralPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Referral from '../Components/Referral';
+
+export default function ReferralPage() {
+  return (
+    <div>
+      <h1>Referral Page</h1>
+      <Referral />
+    </div>
+  );
+}
