34c6f2059428f1e2b35b52ace0fc4dacdf886ea3
Anezijane <ahnnieanezi@gmail.com>
2024-03-11 13:09:31 +0100

    update path to /referral and Component to referral page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ReferralPage from './pages/ReferralPage';
 
@@ -7,9 +7,9 @@ import ReferralPage from './pages/ReferralPage';
 function App() {
     return (
         <Router>
-            <Switch>
-                <Route path="/referral" component={ReferralPage} />
-            </Switch>
+                <Routes>
+                    <Route path="/referral" Component={ReferralPage} />
+                </Routes>
         </Router>
     );
 }
