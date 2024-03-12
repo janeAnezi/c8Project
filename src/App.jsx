7be1783c99b855f5155fff750ffbc2dabb0a32b7
Edozie Magdalene <edoziemagdalene@gmail.com>
2024-03-12 20:07:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MealPlanPage from "./pages/MealPlanPage";
 import MealPointsPage from "./pages/MealPointsPage";
 import MealHistoryPage from "./pages/MealHistoryPage";
-import ReferralPage from "./pages/ReferralPage";
+//import ReferralPage from "./pages/ReferralPage";
 import Onboarding from "./pages/Onboarding";
 import MealPlanPoints from './Components/MealPlanPoints';
 
@@ -16,7 +16,7 @@ function App() {
           <Route path="/MealPoint" element={<MealPointsPage />} />
           <Route path="/mealplan" element={<MealPlanPage />} />
           <Route path="/history" element={<MealHistoryPage />} />
-          <Route path="/referral" element={ReferralPage} />
+         // <Route path="/referral" element={ReferralPage} />
      
            <Route path="/MealPoint" Component={MealPlanPoints} />
 
