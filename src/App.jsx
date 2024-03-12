a13734929d8929a3510096e1fafd9257a1bfc741
Ayomikun <ayoadeosun10@gmail.com>
2024-03-12 10:06:27 +0100

    added get started screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-
-import MealPlanPage from './pages/MealPlanPage';
-import MealPointsPage from './pages/MealPointsPage';
-import MealHistoryPage from './pages/MealHistoryPage';
-
-import ReferralPage from './pages/ReferralPage';
-
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import MealPlanPage from "./pages/MealPlanPage";
+import MealPointsPage from "./pages/MealPointsPage";
+import MealHistoryPage from "./pages/MealHistoryPage";
+import ReferralPage from "./pages/ReferralPage";
+import Onboarding from "./pages/Onboarding";
 
 function App() {
-    return (
-
-        <Router>
-            <Routes>
-                <Route path="/referral" element={<MealPointsPage />} />
-                <Route path="/mealplan" element={<MealPlanPage />} />
-                <Route path="/history" element={<MealHistoryPage />} />
-                                  <Route path="/referral" Component={ReferralPage} />
-
-            </Routes> 
-        </Router> 
-        
-
-      
-    );
+  return (
+    <div className="font-[Manrope]">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Onboarding />} />
+          <Route path="/referral" element={<MealPointsPage />} />
+          <Route path="/mealplan" element={<MealPlanPage />} />
+          <Route path="/history" element={<MealHistoryPage />} />
+          <Route path="/referral" Component={ReferralPage} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
-
-
---
+++ src/pages/Onboarding.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+import GetStarted from "../Components/GetStarted.jsx";
+// import AgeAndBMI from "../Components/AgeAndBMI.jsx";
+// import DietSelection from "../components/DietSelection";
+
+const Onboarding = () => {
+  //   const [selectedScreen, setSelectedScreen] = useState("welcome");
+
+  // const handleGetStartedClick = () => {
+  //   setSelectedScreen("ageAndBmi");
+  // };
+  //   return (
+  //     <>
+  //       {selectedScreen === "welcome" && (
+  //         <GetStarted onClick={handleGetStartedClick} />
+  //       )}
+  //       {selectedScreen === "dietSelection" && <DietSelection />}
+  //     </>
+  //   );
+
+  const [currentPage, setCurrentPage] = useState("getStarted");
+
+  const changeScreen = () => {
+    setCurrentPage("ageAndBmi");
+  };
+
+  return (
+    <div className="]">
+      {currentPage === "getStarted" && <GetStarted onClick={changeScreen} />}
+      {/* {currentPage === "ageAndBmi" && <AgeAndBMI />} */}
+    </div>
+  );
+};
+
+export default Onboarding;
