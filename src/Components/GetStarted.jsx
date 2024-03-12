1aedab5e401c43ab452d498fdd4f49e526b09ee2
Ayomikun <ayoadeosun10@gmail.com>
2024-03-12 10:11:56 +0100

    added background image to get started screen

--- src/Components/GetStarted.jsx
+++ src/Components/GetStarted.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-// import image from "../assets/foodplan.jpg";
+import image from "../assets/foodplan.jpg";
 
 const GetStarted = ({ onClick }) => {
   return (
     <div
       className="min-h-screen flex flex-col justify-center items-center"
-      //   style={{
-      //     background: `linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(${image})`,
-      //     backgroundSize: "cover",
-      //     backgroundPosition: `center`,
-      //   }}
+      style={{
+        background: `linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(${image})`,
+        backgroundSize: "cover",
+        backgroundPosition: `center`,
+      }}
     >
-      <h1 className="text-3xl font-bold mb-4">MealPal</h1>
-      <p className="text-gray-600 mb-6 text-center">
+      <h1 className="text-3xl font-bold mb-4 text-white">MealPal</h1>
+      <p className="text-gray-300 mb-6 text-center">
         Discover Amazing Meal Plans to put you in the pink of health
       </p>
       <button
@@ -21,7 +21,7 @@ const GetStarted = ({ onClick }) => {
       >
         Get Started
       </button>
-      <p className="text-gray-500 mt-4">
+      <p className="text-gray-400 mt-4">
         Already have an account?{" "}
         <span>
           <a href="" className="text-blue-500 hover:text-blue-600">
