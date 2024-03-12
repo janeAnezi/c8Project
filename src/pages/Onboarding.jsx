1b0cc30966b9f075e5bf1ddc0f3cdebbf3857fda
Ayomikun <ayoadeosun10@gmail.com>
2024-03-12 21:05:36 +0100

    added dislikes screen

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import GetStarted from "../Components/GetStarted.jsx";
 import AgeAndBMI from "../Components/AgeAndBMI.jsx";
+import Dislikes from "../Components/Dislikes.jsx";
 // import DietSelection from "../components/DietSelection";
 
 const Onboarding = () => {
@@ -18,16 +19,40 @@ const Onboarding = () => {
   //     </>
   //   );
 
-  const [currentPage, setCurrentPage] = useState("getStarted");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showPage, setShowPage] = useState(false);
+
+  useEffect(() => {
+    setShowPage(true);
+  }, [currentPage]);
 
   const changeScreen = () => {
-    setCurrentPage("ageAndBmi");
+    setShowPage(false);
+    setTimeout(() => {
+      setCurrentPage(currentPage + 1);
+    }, 700);
   };
 
   return (
-    <div className="]">
-      {currentPage === "getStarted" && <GetStarted onClick={changeScreen} />}
-      {currentPage === "ageAndBmi" && <AgeAndBMI />}
+    <div className="flex- flex-col items-center justify-center">
+      <div
+        className={`transition-opacity duration-700 ${
+          showPage ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        {currentPage === 1 && <GetStarted onNext={changeScreen} />}
+        {currentPage === 2 && <AgeAndBMI />}
+        {currentPage === 3 && <Dislikes />}
+      </div>
+
+      {currentPage > 1 && (
+        <button
+          onClick={changeScreen}
+          className="mt-4 px-8 py-2 bg-blue-500 text-white rounded fixed bottom-0 right-0 m-4"
+        >
+          Next
+        </button>
+      )}
     </div>
   );
 };
