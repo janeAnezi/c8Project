a4f2a97dbc69328a203fb485dc8d1a3e6bebe2ea
Adereb-web <123363757+Adereb-web@users.noreply.github.com>
2024-03-13 16:41:50 +0100

    Add files via upload
    Cohort 8 Projects

---
+++ src/Components/MealOptions.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MealOptions =  ({ name }) => {
+    return (
+      <block className="min-h-screen flex items-center justify-center">
+        {name}
+      </block>
+    );
+  };
+
+export default MealOptions;
