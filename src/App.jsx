7f7308a6d50a2a51eb0f5218df99f2d491009ff0
sarah lutaaya <sarahlory2@gmail.com>
2024-03-15 10:09:43 +0300

    import App.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MealPlanPage from "./pages/MealPlanPage";
 import MealPointsPage from "./pages/MealPointsPage";
 import MealHistoryPage from "./pages/MealHistoryPage";
-//import ReferralPage from "./pages/ReferralPage";
+import "./App.css"
+
 import Onboarding from "./pages/Onboarding";
 import MealPlanPoints from "./Components/MealPlanPoints";
 
@@ -15,7 +16,7 @@ function App() {
           <Route path="/MealPoint" element={<MealPointsPage />} />
           <Route path="/mealplan" element={<MealPlanPage />} />
           <Route path="/history" element={<MealHistoryPage />} />
-          {/* <Route path="/referral" element={ReferralPage} /> */}
+          
           <Route path="/MealPoint" Component={MealPlanPoints} />
         </Routes>
       </Router>
