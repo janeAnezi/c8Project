b6700352aa89f1ad34eb405fdee99cf80a6ce5a7
miyemicodes <lizzymiyemi@gmail.com>
2024-03-16 00:07:36 -0700

    initial commit

--- src/Components/MealListing.jsx
+++ src/Components/MealListing.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-import { useEffect } from "react";
-import { useState } from "react";
-// import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 function MealListing() {
@@ -15,26 +12,73 @@ function MealListing() {
 
   return (
     <>
-      <h2 className="text-[#101010] text-base font-semibold">Meal Listing</h2>
-      <div className="flex align-center justify-between ">
-        <div className="flex justify-between">
-          <img
-            src=""
-            className="w-[60px] h-[60px] object-contain"
-            alt=""
-            loading="lazy"
-          />
-          <p className="text-base"> </p>
-        </div>
+      <h2 className="flex content-start text-[#101010] text-base font-semibold ">
+        Meal listing
+      </h2>
+      <ul className="flex flex-col gap-2 font-semibold">
+        <li className="flex flex-row items-center justify-between">
+          <div className="flex gap-2">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/02/06/15/51/steak-626206_1280.jpg"
+              className="w-[50px] h-[50px] object-contain"
+              alt=""
+              loading="lazy"
+            />
+            <p className="text-[11px] flex items-center">Nigerian Jollof </p>
+          </div>
 
-        <button
-          onClick={getMealDetails}
-          className="flex align-center justify-center gap-2 "
-        >
-          <span>View</span>
-          <MdOutlineKeyboardArrowRight className="text-xxl" />
-        </button>
-      </div>
+          <button
+            onClick={getMealDetails}
+            className="flex items-center justify-center gap-2 "
+          >
+            <span className="text-[12px]">View</span>
+            <MdOutlineKeyboardArrowRight className="text-xxl" />
+          </button>
+        </li>
+        <hr />
+
+        <li className="flex flex-row  items-center justify-between">
+          <div className="flex gap-2 ">
+            <img
+              src="https://cdn.pixabay.com/photo/2024/03/11/15/59/ramen-8627028_960_720.jpg"
+              className="w-[50px] h-[50px] object-contain"
+              alt=""
+              loading="lazy"
+            />
+            <p className="text-[11px] flex items-center">Cabbage Stir Fry </p>
+          </div>
+
+          <button
+            onClick={getMealDetails}
+            className="flex align-center justify-center gap-2 "
+          >
+            <span className="text-[12px]">View</span>
+            <MdOutlineKeyboardArrowRight className="text-xxl" />
+          </button>
+        </li>
+        <hr />
+        <li className="flex flex-row  items-center justify-between ">
+          <div className="flex gap-1">
+            <img
+              src="https://cdn.pixabay.com/photo/2019/06/09/23/58/spaghetti-4263260_960_720.jpg"
+              className="w-[50px] h-[50px] object-contain"
+              alt=""
+              loading="lazy"
+            />
+            <p className="text-[11px] flex items-center ">
+              Cauliflower Fried spaghetti
+            </p>
+          </div>
+
+          <button
+            onClick={getMealDetails}
+            className="flex align-center justify-center gap-1"
+          >
+            <span className="text-[12px]">View</span>
+            <MdOutlineKeyboardArrowRight className="text-xxl" />
+          </button>
+        </li>
+      </ul>
     </>
   );
 }
--- src/pages/Previewmeal.jsx
+++ src/pages/Previewmeal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import image from "../assets/foodplan.jpg";
 import MealListing from "./../Components/MealListing";
 
 function PreviewPage() {
@@ -26,37 +27,72 @@ function PreviewPage() {
 */
 
   return (
-    <div className="flex flex-col py-10 align-center justify-center">
-      <div className="flex flex-col">
-        <div className="">
+    <div className="flex flex-col gap-3 bg-white w-full">
+      <div className="w-[100%] ">
+        <div className="w-full flex flex-col gap-2">
           <img
-            src=""
-            className="w-full h-[60px] rounded-lg object-contain"
-            alt=""
-            loading="lazy"
+            src={image}
+            className=" h-[100px] w-full rounded-lg object-cover"
+            alt="A Grilled Salmon with Lemon-Dill Sauce"
           />
 
-          <p className="text-base"> </p>
+          <h3 className="flex align-left text-[12px] font-semibold">
+            Grilled Salmon with Lemon-Dill Sauce
+          </h3>
 
-          <div className="flex gap-6 text-sm">
-            <span className="bg-[#F0F6FF]"> Popular</span>
-            <span className="bg-[#FFF0F0]">2 weeks plan</span>
+          <div className="flex gap-6 text-[10px] font-semibold">
+            <span className="bg-[#F0F6FF] p-1 rounded-md"> Popular</span>
+            <span className="bg-[#FFF0F0] p-1 rounded-md">2 weeks plan</span>
           </div>
 
           <div
-            className=""
-            //dangerouslySetInnerHTML={{ __html: singleMeal?.summary }} for text if generated text is too lenghty "line-clamp-2 hover:line-clamp-none"
-          ></div>
-        </div>
+          //dangerouslySetInnerHTML={{ __html: singleMeal?.summary }} for text if generated text is too lenghty "line-clamp-2 hover:line-clamp-none"
+          >
+            <p className="text-[12px] text-[#282828] indent-0">
+              Succulent grilled salmon fillets, perfectly seasoned and
+              accompanied by a zesty lemon-dill sauce.
+            </p>
+          </div>
 
-        <div>{/**calories */}</div>
+          <ul className="flex flex-row flex-wrap gap-3 text-[12px]">
+            <li>
+              <p>
+                Calories:
+                <span className="font-semibold"> 250g</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                Fat :<span className="font-semibold">10g</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                Sugars: <span className="font-semibold">11g </span>
+              </p>
+            </li>
+            <li>
+              <p>
+                Carb: <span className="font-semibold">12g </span>
+              </p>
+            </li>
+            <li>
+              <p>
+                Sugars: <span className="font-semibold">11g</span>
+              </p>
+            </li>
+          </ul>
+        </div>
       </div>
 
+      <hr/>
+
       <MealListing />
+
       <div className="flex align-center justify-center gap-2 ">
         <button
           type="btn"
-          className="px-2 py-1 bg-[#4268FB] text-white rounded-md text-sm"
+          className="px-4 py-1 bg-[#4268FB] text-white rounded-md text-sm"
         >
           Track progress
         </button>
