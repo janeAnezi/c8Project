cbb4dbf5a231e8d8a0d291023b85fada0f9026dd
Ayomii1 <152004472+Ayomii1@users.noreply.github.com>
2024-03-18 22:19:43 +0100

    Update RecommendedMealPlans.jsx

--- src/pages/RecommendedMealPlans.jsx
+++ src/pages/RecommendedMealPlans.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import MealPlan from "../Components/RecommendedMealPlans";
 
+export default function RecommendedMealPlans() {
+  return (
+    <>
+      <RecommendedMealPlans />
+    </>
+  );
+}
