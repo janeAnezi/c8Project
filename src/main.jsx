41e13168de2fa8df799ed8558731c3e162fce022
Maggy <edoziemagdalene@gmail.com>
2024-03-18 08:49:36 +0100

    fixed main.js file

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import SignUp from './Components/SignUp.jsx'
-// import (RouterProvider, createRoutesFromElements, createBrowserRouter)
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route path='SignUp' element={<SignUp />} />
-      {/* <Route path='SignUp' element={<SignIn />} /> */}
-      {/* <Route path='/' element={<Protected />} /> */}
-    </Route>
-  )
-)
-
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
