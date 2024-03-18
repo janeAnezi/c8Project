ffb2918382df0b72586efc0c20c73202ca9113e5
Ayomii1 <152004472+Ayomii1@users.noreply.github.com>
2024-03-18 23:13:11 +0100

    Update RecommendedMealPlans.jsx

--- src/Components/RecommendedMealPlans.jsx
+++ src/Components/RecommendedMealPlans.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function RecommendedMreturnealPlans() {
   return (
     <>
-    <h1 className="text-3xl text-black-700>Recommended Meal Plans</h1>
+    <h1 className="text-3xl text-black-700"> Recommended Meal Plans </h1>
     <div></div>
     </>
   
