346b2fce89fb56d1bcd2951d94a7c2579726c729
Ayomii1 <152004472+Ayomii1@users.noreply.github.com>
2024-03-19 06:49:16 +0100

    Update RecommendedMealPlans.jsx

--- src/Components/RecommendedMealPlans.jsx
+++ src/Components/RecommendedMealPlans.jsx
@@ -23,6 +23,23 @@ function RecommendedMealPlans() {
             
           </div>
          </li>
+         <hr/>
+         <li className="flex flex-row items-center justify-between">
+          <div className="flex gap-2">
+            <img
+              src="https://cdn.pixabay.com/photo/2021/06/21/15/03/salmon-6353898_960_720.jpg"
+              className="w-[80px] h-[80px] object-contain"
+              alt=""
+              loading="lazy"
+            />
+            <p className="text-lg flex items-start text-black-600 font-Manrope font-semibold">Grilled Salmon with Lemon-Dill Sauce </p>
+             <div className="flex gap-6 text-[10px] font-semibold">
+            <span className="bg-[#F0F6FF] p-1 rounded-md"> Popular</span>
+            <span className="bg-[#FFF0F0] p-1 rounded-md">Vegan Only</span>
+            </div>
+            
+          </div>
+         </li>
          </ul>
     </>
       );
