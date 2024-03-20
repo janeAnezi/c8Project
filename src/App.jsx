7d2f02c66c3260a60778d7e7fdb2431423833560
sarah lutaaya <sarahlory2@gmail.com>
2024-03-21 01:16:17 +0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./Contexts/AuthContext";
 import PrivateRoute from "./Contexts/PrivateRoute";
 import MealPlanPage from "./pages/MealPlanPage";
+import MealPointsPage from "./pages/MealPointsPage";
 import MealHistoryPage from "./pages/MealHistoryPage";
 import "./App.css";
+import ReferralPage from "./pages/ReferralPage";
 import Onboarding from "./pages/Onboarding";
 import Preview from "./pages/Previewmeal";
 import Mealdetails from "./pages/MealFullDetails";
@@ -11,22 +13,19 @@ import SignUp from "./pages/SignUp";
 import RecommendedMeal from "./pages/RecommendedMeal";
 import SignIn from "./pages/SignIn";
 import HomePage from "./pages/HomePage";
-import CommunityPage from "./pages/CommunityPage";
-import Savedmeal from "./pages/Savedmeal";
-
 
 
 function App() {
   return (
     <div className="font-[Manrope]">
-      
       <Router>
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Onboarding />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/signin" element={<SignIn />} />
-            <Route path="/MealPoint" element={<MealPointsPage />} />
+            <Route path="/MealPoint" element={<MealPointsPage />}/>
+            
             <Route
               path="/mealplan"
               element={
@@ -52,7 +51,7 @@ function App() {
               }
             />
             <Route
-              path="/mealDetails/:id"
+              path="/MealDetails/:id"
               element={
                 <PrivateRoute>
                   <Mealdetails />
@@ -60,27 +59,10 @@ function App() {
               }
             />
             <Route
-              path="/saved"
-              element={
-                <PrivateRoute>
-                  <Savedmeal />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/Recommended" element={<RecommendedMeal />} />
-            <Route
               path="/referral"
               element={
-                
-                  <ReferralPage />
-               
-              }
-            />
-            <Route
-              path="/communitypage"
-              element={
                 <PrivateRoute>
-                  <CommunityPage />
+                  <ReferralPage />
                 </PrivateRoute>
               }
             />
@@ -100,7 +82,6 @@ function App() {
                 </PrivateRoute>
               }
             />
-
           </Routes>
         </AuthProvider>
       </Router>
