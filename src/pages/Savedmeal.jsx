ce8f1dca36b8ce4a1cb527ad5ee9cd7e2262db23
miyemicodes <lizzymiyemi@gmail.com>
2024-03-20 14:42:07 -0700

    saved page

--- src/pages/Savedmeal.jsx
+++ src/pages/Savedmeal.jsx
@@ -1,62 +1,34 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { GoDotFill } from "react-icons/go";
+import React from "react";
 
-const apiKey = "38ff337b4439431586d2141d7072a80e";
+//const apiKey = "38ff337b4439431586d2141d7072a80e";
 
 function Savedmeals() {
-  const [meals, setMeals] = useState([]);
-  
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const request = await fetch(
-          `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&number=5&offset=0`
-        );
-
-        const data = await request.json();
-
-        setMeals(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getData();
-  }, []);
-
   return (
     <>
       <div className="flex flex-col gap-3 px-4 py-6">
         <h2 className="text-[#101010] text-base font-bold">Saved Meal Plans</h2>
 
         <div className="w-full flex flex-col items-center gap-2">
-          {meals?.map((singleMeal, index) => (
-            <div
-              className="flex items-center gap-2 w-full"
-              key={`${singleMeal?.id}-${index}`}
-            >
-              <div className="w-[60px] h-[60px] overflow-hidden rounded-md shrink-0">
-                <img
-                  src={singleMeal.image}
-                  className="w-[60px] h-[0px] object-contain"
-                  alt={singleMeal?.title}
-                  loading="lazy"
-                />
-              </div>
+          <div className="flex items-center gap-2 w-full">
+            <div className="w-[60px] h-[60px] overflow-hidden rounded-md shrink-0">
+              <img
+                src=""
+                className="w-[60px] h-[0px] object-contain"
+                alt="Meal name"
+                loading="lazy"
+              />
+            </div>
 
-              <div className="flex flex-col gap-1 items-start flex-shrink flex-1">
-                <p className="text-base line-clamp-1"> {singleMeal?.title}</p>
+            <div className="flex flex-col gap-1 items-start flex-shrink flex-1">
+              <p className="text-base line-clamp-1"> Single food</p>
 
-                <div className="flex gap-3 items-start text-sm">
-                  <span className="">Popular</span>
-                  <GoDotFill className="text-slate-300" />
-                  <span>Vegan Only</span>
-                </div>
+              <div className="flex gap-3 items-start text-sm">
+                <span className="">Popular</span>
+                <GoDotFill className="text-slate-300" />
+                <span>Vegan Only</span>
               </div>
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </>
