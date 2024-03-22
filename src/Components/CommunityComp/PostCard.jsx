17e6102ed8ab7fc69c1b913fc8f6a595fe50df55
ESTHER-OLA <estherolowo2018@gmail.com>
2024-03-22 20:13:50 +0100

    Initial commit message

--- src/Components/CommunityComp/PostCard.jsx
+++ src/Components/CommunityComp/PostCard.jsx
@@ -20,9 +20,10 @@ import {
 } from "firebase/firestore";
 import { db } from "../../firebase/firebase";
 import CommentSection from "./CommentSection";
+import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
-const PostCard = ({ id, logo, email, text, image, timestamp }) => {
+const PostCard = ({ id, name, logo, email, text, image, timestamp }) => {
   const { currentUser, user } = useContext(AuthContext);
   const [state, dispatch] = useReducer(PostsReducer, postsStates);
   const likesRef = doc(collection(db, "posts", id, "likes"));
@@ -95,21 +96,40 @@ const PostCard = ({ id, logo, email, text, image, timestamp }) => {
 
   const commentCount = comments.length;
 
+  const handleNavigateToProfile = () => {
+    console.log("Navigating to user profile page");
+    history.push("/profile");
+  };
+
   return (
     <div className="mb-4">
       <div className="flex flex-col border border-white-300 shadow-md py-4 bg-white rounded-t-3xl">
         <div className="flex items-center pb-4 ml-2">
-          <div className="flex -space-x-1 overflow-hidden">
+          <Link to="/profile" onClick={handleNavigateToProfile}>
+            <div className="flex -space-x-1 overflow-hidden">
+              <img
+                className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+                src={avatar || image}
+                alt="image"
+              />
+            </div>
+          </Link>
+          {/* <div className="flex -space-x-1 overflow-hidden">
             <img
               className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
               src={logo || avatar}
               alt="image"
             />
-          </div>
+          </div> */}
           <div className="flex justify-between w-full">
-            <p className="ml-4 font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
-              {email}
-            </p>
+            <div>
+              <p className="ml-4 font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
+                {name}
+              </p>
+              <p className="ml-4 font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
+                {email}
+              </p>
+            </div>
             <p className="mr-4 font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
               {timestamp}
             </p>
